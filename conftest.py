dfc85a2af10f5a8c2cf93936106e2a4a9dc0e683
00059362 <a00059362@MacBook-Pro-00059362.local>
2025-07-09 15:48:52 +0500

    added new fixture

--- conftest.py
+++ conftest.py
@@ -2,6 +2,12 @@ import pytest
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
+from pages.login_page import LoginPage
+from pages.otp_page import OtpPage
+from pages.permissions_page import PermissionsPage
+from pages.pin_page import PinPage
+
+
 @pytest.fixture(scope='session')
 def driver():
     options = UiAutomator2Options()
@@ -10,10 +16,12 @@ def driver():
     options.app_package = "kz.halyk.onlinebank.stage"
     options.app_activity = "kz.halyk.onlinebank.ui_release4.screens.splash.SplashActivity"
     # options.app = r"C:\Users\Kanat\AndroidStudioProjects\b2b\app\build\outputs\apk\debug\app-debug.apk"
-    options.app = r"/Users/a00059362/AndroidStudioProjects/mobiledemo/app/build/intermediates/apk/debug/app-stage-debug.apk"
+    options.app = r"/Users/a00059362/AndroidStudioProjects/mobiledemo/app.apk"
     options.automation_name = "UiAutomator2"
     options.auto_grant_permissions = True
-    options.app_wait_activity = "*"
+    options.adb_exec_timeout = 60000  # 60 секунд (в миллисекундах)
+    options.uiautomator2_server_launch_timeout = 60000  # 60 секунд
+    # options.app_wait_activity = "*"
 
     driver = webdriver.Remote('http://localhost:4723', options=options)
     yield driver
@@ -21,7 +29,6 @@ def driver():
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_exception_interact(node, call, report):
-    # Проверяем, что это действительно функция с фикстурами
     driver = getattr(node, "funcargs", {}).get("driver", None)
     if driver:
         import os
@@ -30,3 +37,26 @@ def pytest_exception_interact(node, call, report):
         driver.save_screenshot(screenshot_name)
         print(f"\n[Screenshot saved]: {screenshot_name}")
     yield
+
+@pytest.fixture(scope='session')
+def login(driver):
+        driver.save_screenshot("screenshots/before_any_actions.png")
+        login = LoginPage(driver)
+        login.enter_phone("7073502010")
+        driver.save_screenshot("screenshots/after_phone.png")
+        login.click_login()
+        driver.save_screenshot("screenshots/after_click.png")
+
+        otp = OtpPage(driver)
+        otp.enter_otp("000000")
+        driver.save_screenshot("screenshots/after_otp.png")
+
+        pin = PinPage(driver)
+        pin.enter_pin("0000")
+        driver.save_screenshot("screenshots/after_pin.png")
+        pin.enter_pin("0000")
+        driver.save_screenshot("screenshots/after_pin2.png")
+
+        permissions = PermissionsPage(driver)
+        permissions.click_next()
+        driver.save_screenshot("screenshots/after_permission.png")
--- pages/main_page_ob.py
+++ pages/main_page_ob.py
@@ -1,3 +1,4 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,9 +11,7 @@ logger = logging.getLogger(__name__)
 
 class MainPage:
     CONTRACT_NAME = (By.ID, "kz.halyk.onlinebank.stage:id/contract_name_text_view")
-    MENU_MORE = (By.ID, "kz.halyk.onlinebank.stage:id/navigation_more")
-    USER_NAME = (By.ID, "kz.halyk.onlinebank.stage:id/tv_name")
-    ONLINE_DUKEN_BTN = (By.XPATH, '//*[@text="OnlineDuken"]')
+    ONLINE_DUKEN_BTN = (By.XPATH, '(//android.view.ViewGroup[@resource-id="kz.halyk.onlinebank.stage:id/container"])[1]')
 
     def __init__(self, driver):
         self.driver = driver
@@ -22,22 +21,33 @@ class MainPage:
             EC.presence_of_element_located((By.ID, "kz.halyk.onlinebank.stage:id/contract_name_text_view"))
         )
 
+    # def dismiss_hint(self):
+    #     size = self.driver.get_window_size()
+    #     x = size['width'] // 2
+    #     y = int(size['height'] * 0.1)
+    #     os.system(f"adb shell input tap {x} {y}")
+
     def dismiss_hint(self):
         size = self.driver.get_window_size()
         x = size['width'] // 2
         y = int(size['height'] * 0.1)
-        os.system(f"adb shell input tap {x} {y}")
+        try:
+            hint = WebDriverWait(self.driver, 2).until(
+                EC.presence_of_element_located((By.ID, "id_подсказки_или_xpath"))
+            )
+            if hint.is_displayed():
+                os.system(f"adb shell input tap {x} {y}")
+                time.sleep(1)
+        except TimeoutException:
+            print("[INFO] Подсказка не найдена, тапать не будем.")
 
     def wait_until_loaded(self, timeout=15):
         return WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located(self.CONTRACT_NAME)
         )
 
-    def get_contract_name(self):
-        return self.wait_until_loaded().text.strip()
-
     def is_text_present(self, expected_text, timeout=10):
-        self.wait_until_loaded(timeout=timeout)
+        self.wait_for_main_page(timeout=timeout)
         time.sleep(1)
         all_text_views = self.driver.find_elements(By.CLASS_NAME, "android.widget.TextView")
         for el in all_text_views:
@@ -47,34 +57,28 @@ class MainPage:
         print(f"[WARNING] '{expected_text}' не найден ни в одном элементе.")
         return False
 
-    def get_contract_name(self):
-        return self.wait_until_loaded().text.strip()
-
-    def open_menu_more(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.MENU_MORE)
-        ).click()
-
     def select_company_by_name(self, name):
-        # Открыть выбор компании
         WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.USER_NAME)
+            EC.element_to_be_clickable(self.CONTRACT_NAME)
         ).click()
-        # Клик по нужной компании (по тексту)
-        elem = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, f'//*[@text="{name}"]'))
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.CONTRACT_NAME)
         )
-        elem.click()
+        if self.is_text_present(name):
+            elems = self.driver.find_elements(By.CLASS_NAME, "android.widget.TextView")
+            for el in elems:
+                if name in el.text.strip():
+                    el.click()
+                    return True
+        return False
 
     def go_to_online_duken(self):
-        # Ждем появления кнопки OnlineDuken и кликаем по ней
         btn = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable(self.ONLINE_DUKEN_BTN)
         )
         btn.click()
 
     def is_marketplace_loaded(self, timeout=15):
-        # Проверяем наличие лого/текста маркета
         return WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located((By.XPATH, '//*[@text="main logo"]'))
         )
